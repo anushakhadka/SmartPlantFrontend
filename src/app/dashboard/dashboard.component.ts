ebd9f0a2f0df7f3b103ed21f1aca4d3875de9f75
Anusha Khadka <aanukhadka4@gmail.com>
2023-11-26 18:37:19 -0500

    Implementation of functionality of backend data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,5 +9,58 @@ import { CommonModule } from '@angular/common';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  constructor(private fileUploadService: DetectingService) {
+  }
+
+  selectedFile: File | null = null;
+  imagePreview: string | ArrayBuffer | null = null;
+  nameOfPlant: ''
+  descriptionOfPlant: ''
+
+
+
+
+  onFileSelected(event: any) {
+    const fileInput = event.target;
+
+    if (fileInput.files && fileInput.files.length > 0) {
+      this.selectedFile = fileInput.files[0];
+
+      // Display image preview
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imagePreview = e.target.result;
+      };
+      reader.readAsDataURL(this.selectedFile);
+      this.fileUploadService.uploadImage(this.selectedFile).subscribe(
+        (response) => {
+          console.log(response)
+          this.nameOfPlant = response.image_url;
+          console.log(this.nameOfPlant)
+        },
+        (error) => {
+          console.error('Error uploading image:', error);
+        }
+      );
+
+      console.log(this.selectedFile)
+    } else {
+      this.selectedFile = null;
+      this.imagePreview = null;
+    }
+  }
+
+  getWelcome() {
+    this.fileUploadService.getData().subscribe(response => {
+      console.log(response)
+    })
+  }
+
+  getDataOfPlant(data: any) {
+    this.fileUploadService.generateData(data).subscribe(response=>{
+      console.log(response)
+      this.descriptionOfPlant = response.response
+    })
+  }
 
 }
