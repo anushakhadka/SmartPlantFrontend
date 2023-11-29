5cf4ae38fff488983b25abb69ace0d909d25daf1
Nishika <sankuri.2@wright.edu>
2023-11-29 13:05:19 -0500

    changes in services

--- src/app/detecting.service.ts
+++ src/app/detecting.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
+export const BASE_API_URL = 'http://18.219.12.79:8000';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,23 +13,23 @@ export class DetectingService {
   uploadImage(imageFile: File): Observable<any> {
     const formData = new FormData();
     formData.append('image', imageFile, imageFile.name);
-    return this.http.post<any>('http://127.0.0.1:8000/imageApi/api/upload-image/', formData);
+    return this.http.post<any>(`${BASE_API_URL}/imageApi/api/upload-image/`, formData);
   }
 
   getData(): any {
-    return this.http.get('http://127.0.0.1:8000/api/welcome/')
+    return this.http.get(`${BASE_API_URL}/api/welcome/`)
   }
 
   generateData(data: any): Observable<any> {
     const headers = { 'Content-Type': 'application/json' };
-    return this.http.post('http://127.0.0.1:8000/chat/api/get-data/', {data}, {headers})
+    return this.http.post(`${BASE_API_URL}/chat/api/get-data/`, {data}, {headers})
   }
   translate(data: any, language: any): Observable<any> {
     const headers = { 'Content-Type': 'application/json' };
-    return this.http.post('http://127.0.0.1:8000/translate/api/translate-data/', [{language}, {data}], {headers})
+    return this.http.post(`${BASE_API_URL}/translate/api/translate-data/`, [{language}, {data}], {headers})
   }
   readText(data: any, language: any): Observable<any> {
     const headers = { 'Content-Type': 'application/json' };
-    return this.http.post('http://127.0.0.1:8000/readText/api/read-text/', [{language}, {data}], {headers})
+    return this.http.post(`${BASE_API_URL}/readText/api/read-text/`, [{language}, {data}], {headers})
   }
 }
